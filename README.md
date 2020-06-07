d92c5476f3021d9651e4079fe68dbfde1e84c1a0
Imran Sayed <imranhsayed@gmail.com>
2020-06-08 00:15:59 +0530

    Add the styles

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # project-galaxy
+
+
+## :rocket: Development
+
+## To start the webpack:
+
+`cd assets`
+`npm run dev`
+
+## Create a branch
+
+#### If you are in assets directory, come out using 
+`cd ..`
+
+#### Create a branch
+`git checkout -b feature/header`
+
+## Push the code
+`git add .`
+`git commit -m "Your Message"`
+`git push origin feature/branch-name`
--- index.php
+++ index.php
@@ -6,8 +6,11 @@
 	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>Project Galaxy</title>
+	<link rel="stylesheet" href="assets/build/css/main.css">
 </head>
 <body>
-
+	<?php include_once 'templates/layouts/header.php'; ?>
+	<?php include_once 'templates/home/banner.php'; ?>
+	<?php include_once 'templates/layouts/footer.php'; ?>
 </body>
 </html>
