bb47b0852f16153dda7cab132a77ed682cf40ba1
Ratan Oberoi <65130962+ratanoberoi@users.noreply.github.com>
2020-06-28 14:07:46 -0400

    “Update”

--- templates/layouts/header.php
+++ templates/layouts/header.php
@@ -1,3 +1,24 @@
-<header class="header">
-	Header
+<header class="header wrapper">
+	<div class="header-left">
+		<div class="logo">
+			<img src="assets/src/images/Bitmap.png" alt="bitmap">
+		</div>
+		<h1>Productly</h1>
+	</div>
+	<nav class="header-right">
+		<ul>
+			<li>
+				<a href="#">Product</a>
+			</li>
+			<li>
+				<a href="#">Product</a>
+			</li>
+			<li>
+				<a href="#">Product</a>
+			</li>
+			<li>
+				<a href="#">Product</a>
+			</li>
+		</ul>
+	</nav>
 </header>
